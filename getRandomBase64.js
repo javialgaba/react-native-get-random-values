2f38734faa5ba4fc65a78b58365f005134f57728
Javier Algaba <javier.algaba@multisafepay.com>
2021-03-31 09:49:37 +0200

    Add support to Expo 41

--- getRandomBase64.js
+++ getRandomBase64.js
@@ -1,1 +1,11 @@
-module.exports = require('react-native').NativeModules.RNGetRandomValues.getRandomBase64
+const { NativeModules } = require("react-native");
+
+let getRandomBase64;
+
+try {
+  getRandomBase64 = NativeModules.ExpoRandom.getRandomBase64String;
+} catch (e) {
+  getRandomBase64 = NativeModules.RNGetRandomValues.getRandomBase64;
+}
+
+module.exports = getRandomBase64;
